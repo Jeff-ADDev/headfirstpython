09fac91f56d13732fe6f8f4090cd818f76f27871
Jeffrey Holmes <jholmes@revlocal.com>
2023-08-15 21:17:34 -0400

    Chapter 3 - Using swimclub
    Import was not straightforward as in book and had to change it

---
+++ chapter_3/process_swim_files.py
@@ -0,0 +1,3 @@
+from swimclub import get_swim_data
+
+print(get_swim_data("Darius-13-100m-Fly.txt"))
---
+++ chapter_3/swimclub.py
@@ -0,0 +1,23 @@
+import statistics
+
+folder = "./chapter_2/swimdata/"
+
+def get_swim_data(fn):
+    """Given the name of a swimmers file, extract all the required data,
+    then return it to the caller as a tuple."""
+    swimmer, age, distance, stroke = fn.removesuffix(".txt").split("-")
+    with open(folder+fn) as df:
+        data = df.readlines()
+    times = data[0].strip().split(",")
+    converts = []
+    for t in times:
+        minutes, rest = t.split(":")
+        seconds, hundredths = rest.split(".")
+        converts.append(int(minutes)*60*100 + int(seconds)*100 + int(hundredths))
+    average = statistics.mean(converts)
+    min_secs, min_hund = str(round(average / 100, 2)).split(".")
+    min_secs = int(min_secs)
+    minutes = min_secs // 60
+    seconds = min_secs % 60
+    average_time = f"{minutes}:{seconds}.{min_hund}"
+    return swimmer, age, distance, stroke, average, average_time, times
